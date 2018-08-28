cb5baff8104135cf79149fc6b8ef2c8ea202d92a
Jason Miller <jason@tomesoftware.com>
2018-08-28 15:04:08 -0400

    Remove commented code and replace interval markers

--- index.js
+++ index.js
@@ -12,21 +12,7 @@ client = redis.createClient();
 client.once('ready', function () {
 
     // Flush Redis DB
-    client.flushdb();
-
-    // // Initialize Chatters
-    // client.get('chat_users', function (err, reply) {
-    //     if (reply) {
-    //         chatters = JSON.parse(reply);
-    //     }
-    // });
-
-    // // Initialize Messages
-    // client.get('chat_app_messages', function (err, reply) {
-    //     if (reply) {
-    //         chat_messages = JSON.parse(reply);
-    //     }
-    // });
+    // client.flushdb();
 });
 
 var port = process.env.PORT || 8080;
@@ -35,12 +21,6 @@ var port = process.env.PORT || 8080;
 http.listen(port, function () {
     console.log('Server Started. Listening on *:' + port);
 });
-
-// // Store people in chatroom
-// var chatters = [];
-
-// // Store messages in chatroom
-// var chat_messages = [];
 
 // Express Middleware
 app.use(express.static('public'));
@@ -55,69 +35,8 @@ app.get('/', function (req, res) {
     });
 });
 
-// // API - Join Chat
-// app.post('/join', function (req, res) {
-//     var username = req.body.username;
-//     if (chatters.indexOf(username) === -1) {
-//         chatters.push(username);
-//         client.set('chat_users', JSON.stringify(chatters));
-//         res.send({
-//             'chatters': chatters,
-//             'status': 'OK'
-//         });
-//     } else {
-//         res.send({
-//             'status': 'FAILED'
-//         });
-//     }
-// });
-
-// // API - Leave Chat
-// app.post('/leave', function (req, res) {
-//     var username = req.body.username;
-//     chatters.splice(chatters.indexOf(username), 1);
-//     client.set('chat_users', JSON.stringify(chatters));
-//     res.send({
-//         'status': 'OK'
-//     });
-// });
-
-// API - Send + Store Message
-// app.post('/send_message', function (req, res) {
-//     var username = req.body.username;
-//     var message = req.body.message;
-//     chat_messages.push({
-//         'sender': username,
-//         'message': message
-//     });
-//     client.set('chat_app_messages', JSON.stringify(chat_messages));
-//     res.send({
-//         'status': 'OK'
-//     });
-// });
-
-// // API - Get Messages
-// app.get('/get_messages', function (req, res) {
-//     res.send(chat_messages);
-// });
-
-// // API - Get Chatters
-// app.get('/get_chatters', function (req, res) {
-//     res.send(chatters);
-// });
-
 // Socket Connection
 // UI Stuff
 io.on('connection', function (socket) {
-
-    // Fire 'send' event for updating Message list in UI
-    socket.on('message', function (data) {
-        io.emit('send', data);
-    });
-
-    // Fire 'count_chatters' for updating Chatter Count in UI
-    socket.on('update_chatter_count', function (data) {
-        io.emit('count_chatters', data);
-    });
 
 });
--- public/js/main.js
+++ public/js/main.js
@@ -29,51 +29,56 @@ $(function () {
             }
 
             // create HTML for psm table
-            // var table = '';
+            var table = '';
 
-            // for (var index = 0; index < headers.length; index++) {
-            //     if (index === 0){
-            //         table += '<thead>';
-            //     }
-            //     table += '<th>';
-            //     table += headers[index];
-            //     table += '</th>';
-            //     if (index === headers.length - 1){
-            //         table += '</thead>';
-            //     }
-            // }
+            for (var index = 0; index < headers.length; index++) {
+                if (index === 0){
+                    table += '<thead>';
+                }
+                table += '<th>';
+                table += headers[index];
+                table += '</th>';
+                if (index === headers.length - 1){
+                    table += '</thead>';
+                }
+            }
 
-            // $.each(dataObj[index], function( index, value ) {
-            //     table += '<td>';
-            //     table += value;
-            //     table += '</td>';               
-            // });
+            $.each(dataObj[index], function( index, value ) {
+                table += '<td>';
+                table += value;
+                table += '</td>';               
+            });
 
-            // for (var index = 0; index < dataObj.length; index++) {
+            for (var index = 0; index < dataObj.length; index++) {
                 
-            //     table += '<tr>';
+                table += '<tr>';
 
-            //     $.each(dataObj[index], function( index, value ) {
-            //         table += '<td>';
-            //         table += value;
-            //         table += '</td>';               
-            //     });
+                $.each(dataObj[index], function( index, value ) {
+                    table += '<td>';
+                    table += value;
+                    table += '</td>';               
+                });
  
-            //     table += '</tr>';
-            // }
+                table += '</tr>';
+            }
 
-            // document.getElementById('psm-data').innerHTML = table;
+            document.getElementById('psm-data').innerHTML = table;
+
+            // add markers to map
+            $.each(dataObj, function( index, psm ) {
+                addMarker(psm);
+            });
 
             // interval
-            var testIndex = 0;
-            const intervalObj = setInterval(() => {
-                if (dataObj.length > testIndex) {
-                    addMarker(dataObj[testIndex]);
-                    testIndex++;
-                } else {
-                    clearInterval(intervalObj);
-                }
-            }, 1500);
+            // var testIndex = 0;
+            // const intervalObj = setInterval(() => {
+            //     if (dataObj.length > testIndex) {
+            //         addMarker(dataObj[testIndex]);
+            //         testIndex++;
+            //     } else {
+            //         clearInterval(intervalObj);
+            //     }
+            // }, 1500);
         };
         // start reading the file. When it is done, calls the onload event defined above.
         reader.readAsBinaryString(fileInput.files[0]);
@@ -125,111 +130,4 @@ $(function () {
         $('#csv').val('');
         document.getElementById('psm-data').innerHTML = '';
     });
-
-    // var chatter_count;
-    // $.get('/get_chatters', function (response) {
-    //     $('.chat-info').text("There are currently " + response.length + " people in the chat room");
-    //     chatter_count = response.length; //update chatter count
-    // });
-
-    // $('#join-chat').click(function () {
-    //     var username = $.trim($('#username').val());
-    //     $.ajax({
-    //         url: '/join',
-    //         type: 'POST',
-    //         data: {
-    //             username: username
-    //         },
-    //         success: function (response) {
-    //             if (response.status == 'OK') { //username doesn't already exists
-    //                 socket.emit('update_chatter_count', {
-    //                     'action': 'increase'
-    //                 });
-    //                 $('.chat').show();
-    //                 $('#leave-chat').data('username', username);
-    //                 $('#send-message').data('username', username);
-    //                 $.get('/get_messages', function (response) {
-    //                     if (response.length > 0) {
-    //                         var message_count = response.length;
-    //                         var html = '';
-    //                         for (var x = 0; x < message_count; x++) {
-    //                             html += "<div class='msg'><div class='user'>" + response[x]['sender'] + "</div><div class='txt'>" + response[x]['message'] + "</div></div>";
-    //                         }
-    //                         $('.messages').html(html);
-    //                     }
-    //                 });
-    //                 $('.join-chat').hide(); //hide the container for joining the chat room.
-    //             } else if (response.status == 'FAILED') { //username already exists
-    //                 alert("Sorry but the username already exists, please choose another one");
-    //                 $('#username').val('').focus();
-    //             }
-    //         }
-    //     });
-    // });
-
-    // $('#leave-chat').click(function () {
-    //     var username = $(this).data('username');
-    //     $.ajax({
-    //         url: '/leave',
-    //         type: 'POST',
-    //         dataType: 'json',
-    //         data: {
-    //             username: username
-    //         },
-    //         success: function (response) {
-    //             if (response.status == 'OK') {
-    //                 socket.emit('message', {
-    //                     'username': username,
-    //                     'message': username + " has left the chat room.."
-    //                 });
-    //                 socket.emit('update_chatter_count', {
-    //                     'action': 'decrease'
-    //                 });
-    //                 $('.chat').hide();
-    //                 $('.join-chat').show();
-    //                 $('#username').val('');
-    //                 alert('You have successfully left the chat room');
-    //             }
-    //         }
-    //     });
-    // });
-
-    // $('#send-message').click(function () {
-    //     var username = $(this).data('username');
-    //     var message = $.trim($('#message').val());
-    //     $.ajax({
-    //         url: '/send_message',
-    //         type: 'POST',
-    //         dataType: 'json',
-    //         data: {
-    //             'username': username,
-    //             'message': message
-    //         },
-    //         success: function (response) {
-    //             if (response.status == 'OK') {
-    //                 socket.emit('message', {
-    //                     'username': username,
-    //                     'message': message
-    //                 });
-    //                 $('#message').val('');
-    //             }
-    //         }
-    //     });
-    // });
-
-    // socket.on('send', function (data) {
-    //     var username = data.username;
-    //     var message = data.message;
-    //     var html = "<div class='msg'><div class='user'>" + username + "</div><div class='txt'>" + message + "</div></div>";
-    //     $('.messages').append(html);
-    // });
-
-    // socket.on('count_chatters', function (data) {
-    //     if (data.action == 'increase') {
-    //         chatter_count++;
-    //     } else {
-    //         chatter_count--;
-    //     }
-    //     $('.chat-info').text("There are currently " + chatter_count + " people in the chat room");
-    // });
 });
