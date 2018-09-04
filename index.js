5c9573faa1b41481293b71399fc0970731775c58
jason <jason@tomesoftware.com>
2018-09-04 12:40:38 +0000

    log data stream from serial

--- index.js
+++ index.js
@@ -15,6 +15,172 @@ client.once('ready', function () {
     // client.flushdb();
 });
 
+var Gpio = require('onoff').Gpio;
+var blinkLED = new Gpio(12, 'out'); // Corresponds to Pin #32
+var onLED = new Gpio(5, 'out');
+//These are only imported if it's this is not a module
+var config;
+var udpClient;
+var fs = require('fs');
+const execSync = require('child_process').execSync;
+
+
+var handlePSM = (psm) => {
+    if(decodePSM(psm) == 0) {
+      blink();
+      sendPSM(psm);
+    }
+}
+var sendPSM = (psm) => {
+  if(udpClient) {
+    udpClient.send(psm, config.socket.port, config.socket.host, (err) => { });
+  }
+}
+
+var decodePSM = (psm) => {
+  if(fs.existsSync('isMsgFrame')) {
+    if(!decodePSM.count) decodePSM.count = 0;
+    decodePSM.count++;
+    var verbose = false;
+    if(decodePSM.count % 50 == 0) {
+      verbose = true;
+      console.log("count: ", decodePSM.count);
+    }
+    //we have psm message frame checker
+    try {
+      execSync(`./isMsgFrame ${psm.toString('hex')} ${verbose ? '-d' : ''}`, {stdio:[0,1,2]});
+    } catch(err) {
+      return 1
+    }
+    return 0;
+  } else {
+    console.log("isMsgFrame doesn't exist, can't check PSM validity");
+    console.log(`got: ${psm.toString('hex')}`);
+    return 0;
+  }
+
+}
+
+var blink = () => {
+  //using functions as objects is a workaround for using static variables
+  // blink.blinkEndInterval;
+
+  blinkLED.writeSync(1)
+  clearTimeout(blink.blinkEndInterval);
+  blink.blinkEndInterval = setTimeout(function () {
+    blinkLED.writeSync(0);
+  }, config.BLINK_LENGTH);
+}
+
+var setupStdinCallback = () => {
+  process.stdin.on('readable', function () {
+    process.stdin.read();
+    console.log("stdin read line");
+
+    var psm_frame = Buffer.from([ 0x00, 0x20, 0x1a, 0x00, 0x00, 0x04, 0x00, 0x02, 0x08, 0x10, 0x08, 0x10, 0x08, 0x4e, 0xf7, 0xf7, 0x91, 0x39, 0xba, 0x86, 0x22, 0xff, 0xff, 0xff, 0xff, 0x03, 0x20, 0x10, 0xe0]);
+    var psm_no_frame = Buffer.from([ 0x00, 0x00, 0x04, 0x00, 0x14, 0x15, 0x09, 0x09, 0x09, 0x08, 0x4E, 0xF7, 0xF7, 0x91, 0x39, 0xBA, 0x86, 0x22, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x50, 0x10, 0xE0 ]);
+
+    //blink();
+    handlePSM(psm_frame);
+  });
+
+}
+var setupSerialPort = () => {
+  var SerialPort = require('serialport')
+  var Readline = require('@serialport/parser-readline')
+  var serialPort = new SerialPort('/dev/ttyACM0', {
+        baudRate: 115200
+  });
+
+  const parser = serialPort.pipe(new Readline({ delimiter: '\r\n' }))
+
+  var measureTiming = [];
+  var timingLen = 100;
+  var timingCur = 0;
+
+  //parser.on('ready', () => console.log('the ready byte sequence has been received'))
+  parser.on('data', (data) => {
+    if(data.substring(0,3) == 'psm') {
+      dataArr = data.split(' ');
+      var len = parseInt(dataArr[1], 16);
+      if(dataArr.length != len+2) {
+        console.log('wrong length array, bad data');
+        return;
+      }
+      var readData = new Uint8Array(len);
+      //console.log('len:', len);
+      for(var i=2; i<len+2; ++i) {
+        //console.log(dataArr[i]);
+        readData[i-2] = parseInt(dataArr[i], 16);
+      }
+      var buf = Buffer.from(readData);
+
+      handlePSM(buf);
+    } else {
+      //some other data?
+      console.log('dat:', data);
+    }
+  });
+}
+
+var setup = () => {
+  onLED.writeSync(1);
+  console.log("Features enabled:");
+
+  if(config.enabled.socket) {
+    udpClient = require('dgram').createSocket('udp4');
+    console.log("    socket reporting enabled");
+  }
+
+  if(config.enabled.serial) {
+    console.log("    serial detection enabled, normal operation");
+    setupSerialPort();
+  }
+
+  if(config.enabled.stdin) {
+    console.log("    stdin detection enabled, press enter to simulate detection");
+    setupStdinCallback();
+  }
+
+  //Blink LED on start
+  console.log("init led on");
+  blinkLED.writeSync(1);
+  setTimeout(function() {
+    blinkLED.writeSync(0);
+    console.log("init led off");
+  }, 500);
+};
+
+module.exports = {
+  setup: setup,
+  //blink: blink,
+};
+
+//On ctrl c, cleanup a bit better than just exiting
+process.on('SIGINT', function () {
+  blinkLED.unexport();
+  onLED.unexport();
+  process.exit();
+});
+
+
+//EXECUTION STARTS HERE
+if (require.main === module) {
+  try {
+    config = require('./config.json');
+    setup();
+  } catch(e) {
+    //json parse failed
+    console.log("parse json failed");
+    console.log(e);
+    setInterval(function () {
+      blinkLED.writeSync(blinkLED.readSync() === 0 ? 1 : 0)
+    }, 100);
+  }
+} else {
+  //called as module
+}
+
 var port = process.env.PORT || 8080;
 
 // Start the Server
