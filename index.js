c38420f87c414941d67f97e18132309a3e3207be
Jason Miller <jason@tomesoftware.com>
2018-09-04 14:59:15 -0400

    minor style fixes

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ client = redis.createClient();
 // Redis Client Ready
 client.once('ready', function () {
 
-    // Flush Redis DB
-    // client.flushdb();
+  // Flush Redis DB
+  // client.flushdb();
 });
 
 var Gpio = require('onoff').Gpio;
@@ -26,33 +26,33 @@ const exec = require('child_process').exec;
 
 
 var handlePSM = (psm) => {
-    if(decodePSM(psm) == 0) {
-      blink();
-      sendPSM(psm);
-    }
+  if (decodePSM(psm) == 0) {
+    blink();
+    sendPSM(psm);
+  }
 }
 var sendPSM = (psm) => {
-  if(udpClient) {
+  if (udpClient) {
     udpClient.send(psm, config.socket.port, config.socket.host, (err) => { });
   }
 }
 
 var decodePSM = (psm) => {
-  if(fs.existsSync('isMsgFrame')) {
-    if(!decodePSM.count) decodePSM.count = 0;
+  if (fs.existsSync('isMsgFrame')) {
+    if (!decodePSM.count) decodePSM.count = 0;
     decodePSM.count++;
     var verbose = false;
-    if(decodePSM.count % 1 == 0) {
+    if (decodePSM.count % 1 == 0) {
       verbose = true;
       console.log("count: ", decodePSM.count);
     }
     //we have psm message frame checker
     try {
-      exec(`./isMsgFrame ${psm.toString('hex')} ${verbose ? '-d' : ''}`, 
-	function (error, stdout, stderr) {
+      exec(`./isMsgFrame ${psm.toString('hex')} ${verbose ? '-d' : ''}`,
+        function (error, stdout, stderr) {
           console.log(stdout);
-      });
- } catch(err) {
+        });
+    } catch (err) {
       return 1
     }
     return 0;
@@ -80,8 +80,8 @@ var setupStdinCallback = () => {
     process.stdin.read();
     console.log("stdin read line");
 
-    var psm_frame = Buffer.from([ 0x00, 0x20, 0x1a, 0x00, 0x00, 0x04, 0x00, 0x02, 0x08, 0x10, 0x08, 0x10, 0x08, 0x4e, 0xf7, 0xf7, 0x91, 0x39, 0xba, 0x86, 0x22, 0xff, 0xff, 0xff, 0xff, 0x03, 0x20, 0x10, 0xe0]);
-    var psm_no_frame = Buffer.from([ 0x00, 0x00, 0x04, 0x00, 0x14, 0x15, 0x09, 0x09, 0x09, 0x08, 0x4E, 0xF7, 0xF7, 0x91, 0x39, 0xBA, 0x86, 0x22, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x50, 0x10, 0xE0 ]);
+    var psm_frame = Buffer.from([0x00, 0x20, 0x1a, 0x00, 0x00, 0x04, 0x00, 0x02, 0x08, 0x10, 0x08, 0x10, 0x08, 0x4e, 0xf7, 0xf7, 0x91, 0x39, 0xba, 0x86, 0x22, 0xff, 0xff, 0xff, 0xff, 0x03, 0x20, 0x10, 0xe0]);
+    var psm_no_frame = Buffer.from([0x00, 0x00, 0x04, 0x00, 0x14, 0x15, 0x09, 0x09, 0x09, 0x08, 0x4E, 0xF7, 0xF7, 0x91, 0x39, 0xBA, 0x86, 0x22, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x50, 0x10, 0xE0]);
 
     //blink();
     handlePSM(psm_frame);
@@ -92,7 +92,7 @@ var setupSerialPort = () => {
   var SerialPort = require('serialport')
   var Readline = require('@serialport/parser-readline')
   var serialPort = new SerialPort('/dev/ttyACM0', {
-        baudRate: 115200
+    baudRate: 115200
   });
 
   const parser = serialPort.pipe(new Readline({ delimiter: '\r\n' }))
@@ -103,18 +103,18 @@ var setupSerialPort = () => {
 
   //parser.on('ready', () => console.log('the ready byte sequence has been received'))
   parser.on('data', (data) => {
-    if(data.substring(0,3) == 'psm') {
+    if (data.substring(0, 3) == 'psm') {
       dataArr = data.split(' ');
       var len = parseInt(dataArr[1], 16);
-      if(dataArr.length != len+2) {
+      if (dataArr.length != len + 2) {
         console.log('wrong length array, bad data');
         return;
       }
       var readData = new Uint8Array(len);
       //console.log('len:', len);
-      for(var i=2; i<len+2; ++i) {
+      for (var i = 2; i < len + 2; ++i) {
         //console.log(dataArr[i]);
-        readData[i-2] = parseInt(dataArr[i], 16);
+        readData[i - 2] = parseInt(dataArr[i], 16);
       }
       var buf = Buffer.from(readData);
 
@@ -130,17 +130,17 @@ var setup = () => {
   onLED.writeSync(1);
   console.log("Features enabled:");
 
-  if(config.enabled.socket) {
+  if (config.enabled.socket) {
     udpClient = require('dgram').createSocket('udp4');
     console.log("    socket reporting enabled");
   }
 
-  if(config.enabled.serial) {
+  if (config.enabled.serial) {
     console.log("    serial detection enabled, normal operation");
     setupSerialPort();
   }
 
-  if(config.enabled.stdin) {
+  if (config.enabled.stdin) {
     console.log("    stdin detection enabled, press enter to simulate detection");
     setupStdinCallback();
   }
@@ -148,7 +148,7 @@ var setup = () => {
   //Blink LED on start
   console.log("init led on");
   blinkLED.writeSync(1);
-  setTimeout(function() {
+  setTimeout(function () {
     blinkLED.writeSync(0);
     console.log("init led off");
   }, 500);
@@ -172,7 +172,7 @@ if (require.main === module) {
   try {
     config = require('./config.json');
     setup();
-  } catch(e) {
+  } catch (e) {
     //json parse failed
     console.log("parse json failed");
     console.log(e);
@@ -188,20 +188,20 @@ var port = process.env.PORT || 8080;
 
 // Start the Server
 http.listen(port, function () {
-    console.log('Server Started. Listening on *:' + port);
+  console.log('Server Started. Listening on *:' + port);
 });
 
 // Express Middleware
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
 
 // Render Main HTML file
 app.get('/', function (req, res) {
-    res.sendFile('views/index.html', {
-        root: __dirname
-    });
+  res.sendFile('views/index.html', {
+    root: __dirname
+  });
 });
 
 // Socket Connection
